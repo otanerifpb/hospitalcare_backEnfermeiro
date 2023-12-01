7d2a8594ccadbe80a7d2a5f69fc54a385ffc148a
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-12-01 03:11:21 -0300

    Atualização Teste

--- src/main/java/br/edu/ifpb/pdist/back/controller/EnfermeiroController.java
+++ src/main/java/br/edu/ifpb/pdist/back/controller/EnfermeiroController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 //@CrossOrigin(origins = "*")
-@CrossOrigin(origins = "https://hospitalview-8171b04dfdb4.herokuapp.com")
+//@CrossOrigin(origins = "https://hospitalview-8171b04dfdb4.herokuapp.com")
 @RequestMapping("/enfermeiro")
 public class EnfermeiroController {
 
@@ -44,12 +44,12 @@ public class EnfermeiroController {
     }
 
     // Rota para acessar o formulário
-    // @RequestMapping("/formEnfermeiro")
-    // public ModelAndView getFormEnfermeiro(Enfermeiro enfermeiro, ModelAndView mav) {
-    //     mav.addObject("enfermeiro", enfermeiro);
-    //     mav.setViewName("enfermeiro/formEnfermeiro");
-    //     return mav;
-    // }
+    @RequestMapping("/formEnfermeiro")
+    public ModelAndView getFormEnfermeiro(Enfermeiro enfermeiro, ModelAndView mav) {
+        mav.addObject("enfermeiro", enfermeiro);
+        mav.setViewName("enfermeiro/formEnfermeiro");
+        return mav;
+    }
 
     // Rota para cadastrar um Enfermeiro no Sitema
     @RequestMapping(value="/save", method = RequestMethod.POST)
